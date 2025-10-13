26cdbcdacf21994a0f61ae606d732128fa7a2196
lbloomfield1 <lbloomfield-1012@outlook.com>
2025-10-13 14:54:36 +0100

    Add files via upload

---
+++ 02.movement/sketch.js
@@ -0,0 +1,21 @@
+let person = 55;
+let cold = false;
+
+function setup() {
+  createCanvas(400, 400);
+  let pet = "cat is big"
+}
+
+function draw() {
+  background(220);
+  ///circle
+   fill (40,40,40)
+  circle(50,200,50)
+  person= height/2;
+  console.log(person)
+  // the text
+  fill (255,0,0)
+  textSize(20)
+  text("Hello World",100,100)
+  
+}
